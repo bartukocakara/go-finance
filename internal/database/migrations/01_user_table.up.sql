6dfacaf5c60353d925b8d1810e64221cc007c2e0
bartukocakara <kocakarabartu@gmail.com>
2022-06-15 17:09:24 +0200

    slq migraitons up&down added

---
+++ internal/database/migrations/01_user_table.up.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email TEXT NOT NULL,
+    password bytea,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP
+);
+
+CREATE UNIQUE INDEX user_email ON users (email);
