d3d9a3440b2bf2f5ec22c004b366f7dd3cad1916
bartukocakara <kocakarabartu@gmail.com>
2022-06-15 17:35:55 +0200

    server starting versioning and build versioning added

--- internal/api/v1/version.go
+++ internal/api/v1/version.go
@@ -1,1 +1,37 @@
 package v1
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"finance-app/internal/config"
+
+	"github.com/sirupsen/logrus"
+)
+
+// API for returning version
+// When server starts, we set version and than use it if necessary.
+
+type ServerVersion struct {
+	Version string `json:"version"`
+}
+
+var versionJSON []byte
+
+func init(){
+	var err error
+	versionJSON, err = json.Marshal(ServerVersion{
+		Version: config.Version,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Serves version information
+func VersionHandler(w http.ResponseWriter, _ *http.Request){
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(versionJSON); err != nil {
+		logrus.WithError(err).Debug("error writing version")
+	}
+}
