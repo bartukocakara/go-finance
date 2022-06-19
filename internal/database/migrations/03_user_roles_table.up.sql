8d5d10d36f638e96122f35877d7b127d8d526eaa
bartukocakara <kocakarabartu@gmail.com>
2022-06-19 14:28:36 +0200

    Db role migration added

---
+++ internal/database/migrations/03_user_roles_table.up.sql
@@ -0,0 +1,16 @@
+-- Create type Roles to avoid incorrect input
+-- We need only 'admin' role
+-- Role 'member' is if user exists in database
+-- If we need more roles we will add them to this ENUM
+CREATE TYPE user_role AS ENUM (
+    'admin'
+);
+
+CREATE TABLE user_roles (
+    user_id UUID REFERENCES users,
+    role user_role NOT NULL,
+    PRIMARY KEY(user_id, role)
+);
+
+CREATE INDEX user_roles_user_user
+    ON user_roles (user_id)
